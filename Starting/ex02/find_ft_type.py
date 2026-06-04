ab1708b0c2691c421607a3d7592018ad4a2fa67b
Sedritiana Razafindranazy <srazafin@student.42antananarivo.mg>
2026-06-04 07:15:19 +0300

    add starting-subject

--- Starting/ex02/find_ft_type.py
+++ Starting/ex02/find_ft_type.py
@@ -1,2 +1,8 @@
 def all_thing_is_obj(object: any) -> int:
-#your code here
+    if object is None:
+        print(f"{object} is {type(object)}")
+        return -1
+    
+    obj_type = type(object).__name__
+    print(f"{object} is {obj_type}")
+    return -1
