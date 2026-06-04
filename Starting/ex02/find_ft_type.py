0e3418b53337fd48e06cf880216a443e35dddfa2
Sedritiana Razafindranazy <srazafin@student.42antananarivo.mg>
2026-06-04 07:26:53 +0300

    ex02 finished

--- Starting/ex02/find_ft_type.py
+++ Starting/ex02/find_ft_type.py
@@ -1,8 +1,19 @@
 def all_thing_is_obj(object: any) -> int:
-    if object is None:
-        print(f"{object} is {type(object)}")
+    if type(object) == list:
+        print(f"List : {type(object)}")
         return -1
-    
-    obj_type = type(object).__name__
-    print(f"{object} is {obj_type}")
-    return -1
+    elif type(object) == tuple:
+        print(f"Tuple : {type(object)}")
+        return -1
+    elif type(object) == set:
+        print(f"Set : {type(object)}")
+        return -1
+    elif type(object) == dict:
+        print(f"Dict : {type(object)}")
+        return -1
+    elif type(object) == str:
+        print(f"{object} is in the kitchen : {type(object)}")
+        return -1
+    else:
+        print("Type not found")
+        return 42
