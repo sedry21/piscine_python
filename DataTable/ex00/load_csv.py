2b245ee4e1b86a3d51fa2c3f6b3f519374c95e2a
sedry21 <113362053+sedry21@users.noreply.github.com>
2026-07-28 12:20:14 +0000

    add DataTable/ex00

--- DataTable/ex00/load_csv.py
+++ DataTable/ex00/load_csv.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
 def load(path: str) -> pd.DataFrame:
-    return pd.read_csv(path)
+    try:
+        dataset = pd.read_csv(path)
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    
+    print(f"Loading dataset of dimensions {dataset.shape}")
+    return dataset
